1cdf1980e11d4943081e36cf65920ca5abe825fa
Ali Paikan <ali.paikan@luxai.com>
2024-08-27 10:22:21 +0200

    changed  'name' filed of Frame to be field automatically and added two examples for video capture and viewer using ImageFrameCV and zmq transport

---
+++ examples/zmq_video_viewer.py
@@ -0,0 +1,51 @@
+import os, sys
+import time
+import cv2
+import numpy as np
+from time import perf_counter
+from collections import deque
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
+from magpie.utils.logger import Logger
+from magpie.nodes.sink_node import SinkNode
+from magpie.transport.zmq.zmq_subscriber import ZMQSubscriber
+from magpie.frames.image import ImageFrameCV
+
+
+class ZmqVideoViewer(SinkNode):
+
+    def setup(self):
+        self.prev_time = None
+        self.fps_values = deque(maxlen=10)
+
+    def process(self):        
+        data = self.stream_reader.read()
+        if not data: return
+        frame =  ImageFrameCV.from_dict(data)
+        image = frame.to_cv_image()
+        # add info 
+        if self.prev_time:
+            fps = 1.0 / (perf_counter() - self.prev_time)
+            self.fps_values.append(fps)
+            avg_fps = int(sum(self.fps_values) / len(self.fps_values))
+            height, width, _ = image.shape
+            position = position = (10, height - 10)
+            cv2.putText(image, f"{width}x{height} {avg_fps}fps", position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+        self.prev_time = perf_counter()
+        # Display the image
+        cv2.imshow(self.name, image)
+        cv2.waitKey(1)
+
+if __name__ == '__main__':
+
+    node = ZmqVideoViewer(name='VideoViewer', 
+                           stream_reader=ZMQSubscriber("tcp://127.0.0.1:5555"))    
+    
+    while True:
+        try:
+            time.sleep(10)
+        except KeyboardInterrupt:
+            break
+    Logger.info("Closing...")
+    node.terminate()        
--- magpie/frames/frame.py
+++ magpie/frames/frame.py
@@ -2,6 +2,7 @@
 from dataclasses import dataclass, field, fields
 from ulid import ULID
 
+# TODO: check if having 'name' is useful
 # NOTE: in python < 3.10, dataclass does not support 'kw_only'
 #       therefore, all inherited class's attributes must have default value
 #       because the Frame class has fields with default value.   
@@ -9,12 +10,12 @@ from ulid import ULID
 class Frame:    
     gid: int = field(default=None)
     id: int = field(default=None)
-    name: str = field(default=None)
+    name: str = field(init=False)
 
     def __post_init__(self):  
         self.gid = self.gid if self.gid else str(ULID())  # TODO: check if it's better to use ULID().bytes. 
         self.id = self.id if self.id else 0
-        self.name = self.name if self.name else self.__class__.__name__
+        self.name = self.__class__.__name__
 
     def __str__(self):
         return f"{self.name}#{self.gid}:{self.id}"
@@ -29,6 +30,6 @@ class Frame:
     @classmethod
     def from_dict(cls, data: dict):
         # Extract the fields except 'name', which will be set in __post_init__
-        field_names = {f.name for f in fields(cls)}
+        field_names = {f.name for f in fields(cls) if f.name != 'name'}
         init_args = {key: value for key, value in data.items() if key in field_names}
         return cls(**init_args)
